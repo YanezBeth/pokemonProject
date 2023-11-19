f5253dc45a2d150700422532a982889f9dbc9795
Beth Yanez <bethyz@byui.edu>
2023-11-19 15:51:37 -0800

    trying to create homepage with random pokemon/type

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,75 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const pokemonListElement = document.getElementById("pokemonList");
+import pokemon from "pokemontcgsdk";
 
-  // Fetch Pokemon data from JSON file
-  fetch("./json/pokeData.json")
-    .then((response) => response.json())
-    .then((data) => {
-      // Render Pokemon cards
-      data.forEach((pokemon) => {
-        const card = createPokemonCard(pokemon);
-        pokemonListElement.appendChild(card);
-      });
+const baseURL = import.meta.env.POKE_URL;
+const apiKEY = import.meta.env.API_KEY;
+
+pokemon.configure({ apiKey: apiKEY });
+
+// Function to fetch and display card data
+function fetchAndDisplayCards() {
+  // Example: Fetch a single card by ID
+  pokemon.card
+    .find("base1-4")
+    .then((card) => {
+      console.log(card.name); // Log the card name
+      // Now, you can display the card information on your webpage
+      // Example: Display in a card format
+      const cardElement = document.createElement("div");
+      cardElement.innerHTML = `
+        <div>
+          <h2>${card.name}</h2>
+          <p>Set: ${card.set.name}</p>
+          <!-- Add more details as needed -->
+        </div>
+      `;
+      document.body.appendChild(cardElement);
     })
-    .catch((error) => console.error("Error fetching Pokemon data:", error));
+    .catch((error) => {
+      console.error("Error fetching card:", error);
+    });
+}
 
-  // Function to create a Pokemon card
-  function createPokemonCard(pokemon) {
-    const card = document.createElement("div");
-    card.classList.add("card");
+// Call the function to fetch and display cards
+//fetchAndDisplayCards();
 
-    const nameElement = document.createElement("h2");
-    nameElement.textContent = pokemon.name;
+// Function to fetch and display 1 card data per type of pokemon
+async function getRandomPokemonByType() {
+  const types = await pokemon.type.all();
+  const randomPokemonByType = {};
 
-    const typeElement = document.createElement("p");
-    typeElement.textContent = `Type: ${pokemon.type}`;
+  for (const type of types) {
+    const cardsOfType = await pokemon.card.where({
+      q: `types:${type.name}`,
+      pageSize: 1,
+      orderBy: "random",
+    });
 
-    const imageElement = document.createElement("img");
-    imageElement.src = pokemon.image;
-    imageElement.alt = pokemon.name;
-
-    card.appendChild(nameElement);
-    card.appendChild(typeElement);
-    card.appendChild(imageElement);
-
-    return card;
+    if (cardsOfType.data.length > 0) {
+      randomPokemonByType[type.name] = cardsOfType.data[0];
+    }
   }
-});
+
+  return randomPokemonByType;
+}
+
+async function displayRandomPokemonOnHomePage() {
+  const homepageContainer = document.getElementById("homepage-container");
+  const randomPokemon = await getRandomPokemonByType();
+
+  for (const [type, pokemonData] of Object.entries(randomPokemon)) {
+    const cardElement = document.createElement("div");
+    cardElement.innerHTML = `
+      <div>
+        <h2>${type}</h2>
+        <img src="${pokemonData.images.small}" alt="${pokemonData.name}">
+        <p>${pokemonData.name}</p>
+        <a href="/type/${type}">View All ${type} Pokémon</a>
+      </div>
+    `;
+    homepageContainer.appendChild(cardElement);
+  }
+}
+
+window.onload = () => {
+  displayRandomPokemonOnHomePage();
+};
