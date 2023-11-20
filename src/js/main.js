62cc50ec03378b5402c095a7cf070f0d136a42e2
Beth Yanez <bethyz@byui.edu>
2023-11-19 16:51:15 -0800

    Got the type list working!

--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,36 @@
 import pokemon from "pokemontcgsdk";
 
-const baseURL = import.meta.env.POKE_URL;
-const apiKEY = import.meta.env.API_KEY;
+const baseURL = import.meta.env.VITE_POKE_URL;
+const apiKey = import.meta.env.VITE_API_KEY;
 
-pokemon.configure({ apiKey: apiKEY });
+pokemon.configure({ apiKey });
 
-// Function to fetch and display card data
-function fetchAndDisplayCards() {
-  // Example: Fetch a single card by ID
-  pokemon.card
-    .find("base1-4")
-    .then((card) => {
-      console.log(card.name); // Log the card name
-      // Now, you can display the card information on your webpage
-      // Example: Display in a card format
-      const cardElement = document.createElement("div");
-      cardElement.innerHTML = `
-        <div>
-          <h2>${card.name}</h2>
-          <p>Set: ${card.set.name}</p>
-          <!-- Add more details as needed -->
-        </div>
-      `;
-      document.body.appendChild(cardElement);
-    })
-    .catch((error) => {
-      console.error("Error fetching card:", error);
-    });
-}
+// Function to fetch and display all Pokémon types
+async function fetchAndDisplayPokemonTypes() {
+  try {
+    // Set the API key in the headers
+    pokemon.configure({ apiKey });
 
-// Call the function to fetch and display cards
-//fetchAndDisplayCards();
+    const types = await pokemon.type.all();
+    console.log("Types response:", types);
 
-// Function to fetch and display 1 card data per type of pokemon
-async function getRandomPokemonByType() {
-  const types = await pokemon.type.all();
-  const randomPokemonByType = {};
+    // Display the types on the webpage
+    const typesContainer = document.getElementById("pokemonTypes");
+    typesContainer.innerHTML = "<h2>All Pokémon Types</h2>";
 
-  for (const type of types) {
-    const cardsOfType = await pokemon.card.where({
-      q: `types:${type.name}`,
-      pageSize: 1,
-      orderBy: "random",
+    const typesList = document.createElement("ul");
+    types.forEach((type) => {
+      // <-- Corrected this line
+      const typeItem = document.createElement("li");
+      typeItem.textContent = type;
+      typesList.appendChild(typeItem);
     });
 
-    if (cardsOfType.data.length > 0) {
-      randomPokemonByType[type.name] = cardsOfType.data[0];
-    }
-  }
-
-  return randomPokemonByType;
-}
-
-async function displayRandomPokemonOnHomePage() {
-  const homepageContainer = document.getElementById("homepage-container");
-  const randomPokemon = await getRandomPokemonByType();
-
-  for (const [type, pokemonData] of Object.entries(randomPokemon)) {
-    const cardElement = document.createElement("div");
-    cardElement.innerHTML = `
-      <div>
-        <h2>${type}</h2>
-        <img src="${pokemonData.images.small}" alt="${pokemonData.name}">
-        <p>${pokemonData.name}</p>
-        <a href="/type/${type}">View All ${type} Pokémon</a>
-      </div>
-    `;
-    homepageContainer.appendChild(cardElement);
+    typesContainer.appendChild(typesList);
+  } catch (error) {
+    console.error("Error fetching Pokémon types:", error);
   }
 }
 
-window.onload = () => {
-  displayRandomPokemonOnHomePage();
-};
+// Call the function to fetch and display Pokémon types
+fetchAndDisplayPokemonTypes();
