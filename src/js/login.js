74856bbc2893fac205bd702a1c68cd98ff4c184e
Beth Yanez <bethyz@byui.edu>
2023-12-08 22:36:31 -0800

    Login:meh,alright. Register:needs work

--- src/js/login.js
+++ src/js/login.js
@@ -1,17 +1,46 @@
-import { login } from "./auth.mjs";
-import { getParam, loadHeaderFooter } from "./utils.mjs";
-import { checkLogin } from "./auth.mjs";
+import {
+  loadHeaderFooter
+} from "./utils.mjs";
 
-loadHeaderFooter();
-console.log(window.location.href);
-let redirect = getParam("redirect");
 
-if (redirect === null) {
-  redirect = "./index.html";
-}
-document.querySelector("#loginButton").addEventListener("click", (e) => {
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
-  console.log(email, password);
-  login({ email, password }, redirect);
+document.addEventListener("DOMContentLoaded", function () {
+  // Load header and footer
+  loadHeaderFooter();
 });
+
+// console.log(window.location.href);
+// let redirect = getParam("redirect");
+
+// if (redirect === null) {
+//   redirect = "./index.html";
+// }
+// document.querySelector("#loginButton").addEventListener("click", (e) => {
+//   const email = document.querySelector("#userEmail").value;
+//   const password = document.querySelector("#userPassword").value;
+//   console.log(email, password);
+//   login({
+//     email,
+//     password
+//   }, redirect);
+// });
+
+
+const loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const username = document.getElementById("userName").value;
+  localStorage.setItem("userName", username); // store username in local storage
+  updateGreeting(username); // update greeting message
+  // After successful login, redirect with message:
+  window.location.href = "./index.html?message=Sign in successful!";
+});
+
+function updateGreeting(username) {
+  const greetingElement = document.getElementById("nav-p");
+  greetingElement.textContent = `Hello, ${username}`;
+}
+
+const storedUsername = localStorage.getItem("userName");
+if (storedUsername) {
+  updateGreeting(storedUsername);
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import pokemon from "pokemontcgsdk";
 import { loadHeaderFooter } from "./utils.mjs";
 
+
 const apiKey = import.meta.env.VITE_API_KEY;
 
 pokemon.configure({
@@ -11,3 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
   // Load header and footer
   loadHeaderFooter();
 });
+
+const urlParams = new URLSearchParams(window.location.search);
+const message = urlParams.get("message");
+
+if (message) {
+  // Display the message
+  const messageElement = document.getElementById("nav-p");
+  messageElement.textContent = message;
+}
